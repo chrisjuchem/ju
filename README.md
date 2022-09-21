2bec9cecf8afc64c6b30f8b54df77dd37d2b2042
Chris Juchem <cjuchem@starry.com>
2022-09-21 14:01:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# ju (Juchem's Utility)
+
+A tool for setting up a new Linux installation with some of my preferred
+settings and aliases.
+
+## Prerequisutes
+
+- `git`, with ssh configured, e.g.
+
+        ssh-keygen -t ed25519 -C "<emailaddress>"
+        ssh-add ~/.ssh/id_ed25519
+  and upload to https://github.com/settings/keys
+- A bashrc that sources all files within `~/.bashrc.d/`, e.g.
+
+        if [ -d ~/.bashrc.d ]; then
+            for rc in ~/.bashrc.d/*; do
+                if [ -f "$rc" ]; then
+                    . "$rc"
+                fi
+            done
+        fi
+
+## Setup 
+
+From your home directory, run:
+
+```sh
+git clone git@github.com:chrisjuchem/ju.git
+~/ju/setup.sh
+```
+
+`setup.sh` is designed to be indempotent, so you can run it again at anytime, after
+making changes or pulling down a newer version.
+
+## Major Components
+
+### `jurc`
+
+Aliases and environment variables to be sourced by `bashrc`. `setup.sh` creates a
+file in `~/bashrc.d/` that sources this file directly, so changes to this file 
+are reflecetd in the next shell opened even without running `setup.sh` again.
+
+### `bin`
+
+Directory containing executables. Added to $PATH by `jurc`.
+
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PURPLE='\033[0;35m'
+RESET='\033[0m' # No Color
+
+
+echo -e "${PURPLE}Beginning Setup...${RESET}"
+
+set -e
+
+JU_DIR=$HOME/ju
+
+echo -e "${PURPLE}Installing rc files...${RESET}"
+mkdir -p ~/.bashrc.d
+echo source ${JU_DIR}/jurc > ~/.bashrc.d/ju
+
+
+if [ ! -d ~/.bash-git-prompt ]; then
+  echo -e "${PURPLE}Installing bash-git-prompt...${RESET}"
+  git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+else
+  echo -e "${PURPLE}bash-git-prompt already installed...${RESET}"
+fi
+
+echo -e "${PURPLE}Configuring git...${RESET}"
+git config --global alias.cln "!f() { git fetch --prune; echo '\"git clnd\" will delete:'; git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}'; }; f"
+git config --global alias.clnd "!f() { git fetch --prune; git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D; }; f"
+git config --global alias.defbranch "!git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'"
+git config --global alias.rbdef "!git rebase origin/\$(git defbranch) -i"
+git config --global alias.home "!git checkout \$(git defbranch) && git pull"
+git config --global alias.tree "log --oneline --all --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(red)- %an%C(reset)%C(bold yellow)%d%C(reset)'"
+git config --global rebase.autosquash true
+git config --global grep.lineNumber true
+git config --global core.excludesFile ${JU_DIR}/.gitignore-global
+
+echo -e "${PURPLE}Done!${RESET}"
