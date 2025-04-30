e61c5317f16a40d18f2d4b1aac729d99256e67d5
Chris Juchem <chrisjuchem15@gmail.com>
2025-04-30 18:38:13 +0000

    display hostnames

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -1,8 +1,9 @@
-# This is an alternative approach. Single line in git repo.
 override_git_prompt_colors() {
   GIT_PROMPT_THEME_NAME="JU"
 
-  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${White}${Time12a}${ResetColor} ${BoldYellow}${PathShort}${ResetColor}"
+  [ -z "${DISPLAY_HOSTNAME}" ] || Host="${Red}${DISPLAY_HOSTNAME}${ResetColor}:"
+
+  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${White}${Time12a}${ResetColor} ${Host}${BoldYellow}${PathShort}${ResetColor}"
   GIT_PROMPT_END_USER="${ResetColor} $ "
   GIT_PROMPT_END_ROOT="${BoldRed} # "
 
